43ce85e995d87c39bc5942464ff3c6c64dab3b94
SyedSammad-123 <syedsammadjamshad@gmail.com>
2025-08-20 19:08:50 +0500

    commit

---
+++ app.js
@@ -0,0 +1,226 @@
+alert("You are Welcome to number Guessing Game!")
+
+var guessnumber = Math.floor(Math.random() * 20) + 1;
+
+var choosenumber = prompt("You are Guess the number between 1 to 20");
+
+if (choosenumber === guessnumber) {
+    document.write("Many Congraulats! Guess the Right Number..")
+} else if (choosenumber > guessnumber) {
+    document.write("To high Closs this Number! "  + guessnumber)
+} else if (choosenumber > guessnumber) {
+    document.write("To low Closs this Number! "  + guessnumber)
+}else{
+    document.write("Try Again Your Guess is incorect!")
+}
+
+
+
+
+
+
+
+// alert("welcome to javascipt")
+
+//q 2
+
+// alert("Error plece enter a valid password")
+
+//q 3
+
+// alert("welcome to jsland \n happy cooding .");
+
+//q 4
+
+// var userName = ""
+
+//q 5
+
+// var MyName = "syed samad shah"
+
+//q 6
+
+// var Message = "Hello word"
+// alert( Message)
+//q 7
+
+// var stname = "syed samad";
+// alert(stname );
+
+// var Age = "16";
+// alert(Age);
+
+// var rollon = "110";
+// alert(rollon);
+
+// var cours = "Web development";
+// alert(cours)
+
+//q 8
+
+// alert("pizza\npizz\npiz\npi\np")
+
+//q 9
+
+// document.write("I can write HTML content throug JAVA SCRIPT")
+
+//q 10
+
+// var BirthYere = 2009
+// document.write("My BirthYere is:" + BirthYere )
+
+//q 11
+
+// var visiter = "syed samad";
+
+// var product = "laptop"
+
+// var quntity = 20
+
+// document.write(visiter +"ordered" + quntity + product + "for on daraz")
+
+//q 12
+
+// var num1 = 5
+// var num2 = 6
+// var result = num1+num2
+// document.write(result + "<br>")
+
+// var num1 = 10
+// var num2 = 5
+// var result = num1-num2
+// document.write(result +"<br>")
+
+// var num1 = 5
+// var num2 = 3
+// var result = num1*num2
+// document.write(result +"<br>")
+
+// var num1 = 10
+// var num2 = 20
+// var result = num1/num2
+// document.write(result +"<br>")
+
+// var num1 = 10
+// var num2 = 30
+// var result = num1%num2
+// document.write(result +"<br>")
+
+//q 13
+
+// //st 1
+// var value = '';
+
+// // st 2
+
+// document.write("value after variable is" + value);
+
+// //st 3
+
+// var value = 25
+
+// //st 4
+
+// document.write("Initial value is" + value +"<br>")
+
+// //st 5
+
+// var  increment = value+10
+
+// //st 6
+
+// document.write("value after icerment is" +increment +"<br>")
+
+// //st 7
+
+// var add = increment+7
+
+// //st 8
+
+// document.write("value after addition is " +add +"<br>")
+
+// //st 9
+// var decrement = add
+
+// //q 14
+
+// var tiketprice = 500;
+// var currency = "PKR"
+// var calculate =  tiketprice * 5;
+// document.write("TOTAL cost buy 5  ticket the movue " +calculate+""+currency);
+// // q 15
+
+// var num = 4
+// document.write( num +"x 1 = "+num*1+"<br>")
+// document.write( num +"x 2 = "+num*2+"<br>")
+// document.write( num +"x 3 = " +num*3+"<br>")
+// document.write( num +"x 4 = "+num*4+"<br>")
+// document.write( num +"x 5 = "+num*5+"<br>")
+// document.write( num +"x 6 = "+num*6+"<br>")
+// document.write( num +"x 7 = "+num*7+"<br>")
+// document.write( num +"x 8 = "+num*8+"<br>")
+// document.write( num +"x 9 = "+num*9+"<br>")
+// document.write( num +"x 10 = "+num*10+"<br>")
+
+
+// q 16
+
+// var userName =prompt("Type your name?")
+// alert("welcome to user" + " " + userName)
+
+//Q 17
+
+// var num = prompt("Type a number")
+//  document.write( num +"x 1 = "+num*1+"<br>")
+//  document.write( num +"x 2 = "+num*2+"<br>")
+//  document.write( num +"x 3 = " +num*3+"<br>")
+//  document.write( num +"x 4 = "+num*4+"<br>")
+//  document.write( num +"x 5 = "+num*5+"<br>")
+//  document.write( num +"x 6 = "+num*6+"<br>")
+//  document.write( num +"x 7 = "+num*7+"<br>")
+//  document.write( num +"x 8 = "+num*8+"<br>")
+//  document.write( num +"x 9 = "+num*9+"<br>")
+//  document.write( num +"x 10 = "+num*10+"<br>")
+
+//q 18
+
+// var city = prompt("Enter your city name")
+// if (city === "Karahi") {
+//     alert("welcome to city of light")
+// }
+
+//q 19
+// var gender = prompt("Tell your gender\nmale or female");
+// if(gender === "male"){
+//     alert("Good morinig sir")
+// }
+// else if(gender === "female") {
+//     alert("good morning mam")
+// }
+// else{
+//     alert("goog morning")
+// }
+
+//q 20
+
+// var signalcolor =prompt("Enter signal color her :\nred\nyellow\ngreen");
+// if(signalcolor==="red") {
+//     alert("stop")
+// }
+// else if(signalcolor==="yellow") {
+//     alert("rade to mave")
+// }
+// else if (signalcolor==="green") {
+//     alert("it,s time to move")
+
+// }else{
+//     alert("Invalid input ")
+// }
+
+
+//q 21
+
+// var fule = prompt("How much liter")
+// if(fule<6) {
+//     alert("pleace fill in the fule")
+// }
